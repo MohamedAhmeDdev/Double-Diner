c82548e5b5911aaee49771ecf85ba012f6d0f7ac
Mohamed Ahmed <ma07041705@gmail.com>
2022-11-21 19:21:07 +0300

    added pdf

--- Client/src/Components/ListOfOrders.js
+++ Client/src/Components/ListOfOrders.js
@@ -1,90 +1,101 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import { ImSpinner6 } from "react-icons/im";
-import '../css/ListOfOrders.css'
+import "../css/ListOfOrders.css";
+import { useReactToPrint } from "react-to-print";
 
 function ListOfOrders() {
-    const [orderList, setOrderList] = useState([])
-    const [customerList, setCustomerList] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [message, setMessage] = useState(false)
+  const [orderList, setOrderList] = useState([]);
+  const [customerList, setCustomerList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(false);
+  const componentRef = useRef();
 
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: "emp-data",
+    onAfterPrint: () => alert("print success"),
+  });
 
-    const getOrderList = async () => {
-        const orderList = await axios.get('http://localhost:5000/orderItems')
-        setOrderList(orderList.data)
-    }
+  const getOrderList = async () => {
+    const orderList = await axios.get("http://localhost:5000/orderItems");
+    setOrderList(orderList.data);
+  };
 
-    const getCustomerList = async () => {
-        const customerInfo = await axios.get('http://localhost:5000/order')
-        setCustomerList(customerInfo.data)
-    }
+  const getCustomerList = async () => {
+    const customerInfo = await axios.get("http://localhost:5000/order");
+    setCustomerList(customerInfo.data);
+  };
 
-    useEffect(() => {
-        getOrderList();
-    }, [orderList]);
+  useEffect(() => {
+    getOrderList();
+  }, [orderList]);
 
-    useEffect(() => {
-        getCustomerList();
-    }, [customerList]);
+  useEffect(() => {
+    getCustomerList();
+  }, [customerList]);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
 
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 4000)
-    }, [])
+  useEffect(() => {
+    setMessage("The Order Is Being Prossed");
+    setTimeout(() => {
+      setMessage(false);
+    }, 3000);
+  }, []);
 
-    useEffect(() => {
-        setMessage("The Order IS Being Prossed")
-        setTimeout(() => {
-            setMessage(false)
-        }, 3000)
-    }, [])
+  return (
+    <div className="container-OrderList">
+      <div>
+        {message && <p className="message-div">{message}</p>}
 
+        {loading ? (
+          <div className="loader">
+            <ImSpinner6 size="4em" />
+          </div>
+        ) : (
+          <>
+            <table className="ListOrders-table" ref={componentRef}>
+              <thead>
+                <tr>
+                  <th>FullName</th>
+                  <th>Phone</th>
+                  <th>Location</th>
+                  <th>Food</th>
+                  <th>Status</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {customerList.map((item, id) => (
+                  <tr key={id}>
+                    <td>{item.fullName}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.location}</td>
 
+                    <td>
+                      {orderList.map((item, id) => (
+                        <>{item.foodName},</>
+                      ))}
+                    </td>
 
-    return (
-        <div className='container-OrderList'>
-            <div>
-            {message && <p className='message-div'>{message}</p>}
-
-            <table className="ListOrders-table">
-        <thead>
-          <tr>
-            <th>FullName</th>
-            <th>Phone</th>
-            <th>Location</th>
-            <th>Food</th>
-            <th>Status</th>
-            <th>Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customerList.map((item, id) => (
-            <tr key={id}>
-              <td>{item.fullName}</td>
-              <td>{item.phone}</td>
-              <td>{item.location}</td>
-
-              <td>
-              {orderList.map((item, id) => (
-                <>
-                 {item.foodName},
-                </>
-              ))}
-              </td>
-
-              <td>{item.status}</td>
-              <td>Ksh {item.priceTotal}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-            </div>
-        </div>
-    )
+                    <td>{item.status}</td>
+                    <td>Ksh {item.priceTotal}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button className="pdf" onClick={handlePrint}>PDF</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default ListOfOrders
+export default ListOfOrders;
