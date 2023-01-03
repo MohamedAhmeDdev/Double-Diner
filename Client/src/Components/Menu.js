9e476d8f35d8f4b63e1e26ae19762d8300515b39
Baraka M Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-01-03 10:20:41 +0300

    Update Menu.js

--- Client/src/Components/Menu.js
+++ Client/src/Components/Menu.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react'
-import '../css/Menu.css'
-import '../css/Meal.css'
-import Meal from './Meal'
-import Juice from './Juice'
-import Shakes from './Shakes'
+import "../css/Menu.css";
+import "../css/Meal.css";
 
-function Home({ cartItems, setCartItems }) {
+import React, { useState } from "react";
 
-    return (
-        <div className='home-container'>
-            <div className='logo'>
-                <div className='quotes'>
-                    <h3 className='logo-name'>Double Diner</h3>
-                    <p className='logo-info'>Food Tastes Better When You Eat With Your Family Or Friends</p>
-                </div>
-            </div>
+import Juice from "./Juice";
+import Meal from "./Meal";
+import Shakes from "./Shakes";
 
-            <div className="menu">
-                <Meal cartItems={cartItems} setCartItems={setCartItems} />
-                <Juice cartItems={cartItems} setCartItems={setCartItems} />
-                <Shakes cartItems={cartItems} setCartItems={setCartItems} />
-            </div>
+const HomePage = ({ cartItems, setCartItems }) => {
+  return (
+    <div className="home-container">
+      <div className="logo">
+        <div className="quotes">
+          <h3 className="logo-name">Double Diner</h3>
+          <p className="logo-info">
+            Food Tastes Better When You Eat With Your Family Or Friends
+          </p>
         </div>
-    )
-}
+      </div>
 
-export default Home
+      {cartItems.length > 0 && (
+        <div className="menu">
+          <Meal cartItems={cartItems} setCartItems={setCartItems} />
+          <Juice cartItems={cartItems} setCartItems={setCartItems} />
+          <Shakes cartItems={cartItems} setCartItems={setCartItems} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
