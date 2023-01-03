42b4dc40ed6cda0227f2525f0db57837c90088fe
Baraka M Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-01-03 10:21:17 +0300

    Move admin dashboard to pages folder

---
+++ Admin/src/pages/AdminDashboard.jsx
@@ -0,0 +1,56 @@
+import "../css/Dashboard.css";
+
+import { BsTable } from "react-icons/bs";
+import { GiMeal } from "react-icons/gi";
+import { GrOrderedList } from "react-icons/gr";
+import { Link } from "react-router-dom";
+import { MdAccountCircle } from "react-icons/md";
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <div className="logo-div">
+        <h3 className="logo">Double Diner</h3>
+      </div>
+      <div className="container">
+        <Link className="Link" to="/orders">
+          <div className="container-div">
+            <p className="icons">
+              <MdAccountCircle color="gold" size="2em" />
+            </p>
+            <p className="grid-name">Orders</p>
+          </div>
+        </Link>
+        <Link className="Link" to="/customers">
+          <div className="container-div">
+            <p className="icons">
+              <GrOrderedList color="gold" size="2em" />
+            </p>
+            <p className="grid-name">Customers</p>
+          </div>
+        </Link>
+
+        <Link className="Link" to="/reservations">
+          <div className="container-div">
+            <p>
+              <BsTable color="gold" size="2em" />
+            </p>
+            <p className="grid-name">Reservations</p>
+          </div>
+        </Link>
+
+        <Link className="Link" to="/inventory">
+          <div className="container-div">
+            <p className="icons">
+              <GiMeal color="gold" size="2em" />
+            </p>
+            <p className="grid-name">Manage Inventory</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
