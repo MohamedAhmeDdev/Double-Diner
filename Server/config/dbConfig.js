c54e9bd4a89c869a1de39d15729950de2d099e52
Mohamed Ahmed <ma07041705@gmail.com>
2023-05-25 10:58:31 +0300

    Remove cached files

--- Server/config/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('sequelize');
-
-const db = new sequelize('double_diner', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-});
-
-module.exports = db
--- Server/constants/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-const JWT_SECRET = process.env.SECRET || "secret";
-
-module.exports = {
-  JWT_SECRET,
-
-};
