3cf51e61543951b406cd8624404c562483f05fc9
Mohamed Ahmed <ma07041705@gmail.com>
2024-01-20 20:03:16 +0300

    added date and time format

--- Client/src/constants/index.js
+++ Client/src/constants/index.js
@@ -1,1 +1,35 @@
 export const SERVER_URL = "https://doublediner-4zwd.onrender.com";
+
+
+export function formatDate(inputDate) {
+    const options = { year: 'numeric', month: 'short', day: '2-digit', hour12: true };
+    const formattedDate = new Date(inputDate).toLocaleDateString('en-US', options);
+    return formattedDate;
+  }
+  
+  export function formatTime(inputTime) {
+    if (!inputTime) {
+      return ""; // Handle invalid or empty input
+    }
+  
+    // Parse the input date string
+    const date = new Date(inputTime);
+  
+    // Get hours, minutes, and AM/PM from the date object
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const period = hours >= 12 ? 'PM' : 'AM';
+  
+    // Convert hours to 12-hour format
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+  
+    // Add leading zero to minutes if needed
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  
+    // Create the formatted time string
+    const formattedTime = `${formattedHours}:${formattedMinutes} ${period}`;
+  
+    return formattedTime;
+  }
+  
+  
