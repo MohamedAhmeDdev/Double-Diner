ae95d35751dda59abd4461b09c9a40845a1c9792
Mohamed Ahmed <ma07041705@gmail.com>
2024-01-22 14:39:26 +0300

    checkout redone

--- Client/src/pages/CheckOutPage.jsx
+++ Client/src/pages/CheckOutPage.jsx
@@ -4,28 +4,9 @@ import { UseAuthContext } from "../hook/UseAuthContext";
 import { UseCartContext } from "../hook/UseCartContext";
 import { apiCall } from "../utils/apiCall";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import validator from "validator";
 
-const OrderedItem = ({ item }) => (
-  <div className="flex items-center justify-between  w-[48rem]">
-    <div className="flex items-center space-x-4">
-      <div className="flex-shrink-0">
-        <img className="w-24 h-24 rounded-md"src={`${SERVER_URL}/${item?.image}`} alt="" />
-      </div>
-
-      <div className="flex-1 min-w-0">
-        <p className="text-xl font-bold  text-gray-900 truncatecapitalize"> {item?.name}</p>
-        <p className="text-gray-600 text-sm truncate  max-w-sm"> {item?.description}</p>
-      </div>
-    </div>
-
-    <div className="flex flex-col ">
-      <span className="text-gray-900 text-lg font-medium"> {item.quantity} x {item?.price}</span>
-    </div>
-    <span className="text-gray-900 text-lg font-medium"> Ksh. {item?.quantity * item?.price}</span>
-  </div>
-);
 
 const CheckOutPage = () => {
   const { cartItems, clear } = UseCartContext();
@@ -38,11 +19,15 @@ const CheckOutPage = () => {
 
   const placeOrder = (e) => {
     e.preventDefault();
-
-    if (!phoneNo) {
+    if (!phoneNo ) {
       toast.error("Please fill in your phone number");
       return;
     }
+    if (!validator.isMobilePhone(phoneNo, "en-KE")) {
+      toast.error("Please enter a valid phone number");
+      return;
+    }
+    
     if (!address) {
       toast.error("Please fill in your address");
       return;
@@ -77,68 +62,65 @@ const CheckOutPage = () => {
   );
 
   return (
-    <div className="container mt-32 flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        {cartItems?.length > 0 ? (
-          <>
-            <p className="text-xl font-bold text-gray-900">Dear{" "}
-              <span className="text-2xl font-bold text-blue-600 capitalize">{user?.name} </span>
-              , with email{" "}
-              <span className="text-2xl font-bold text-blue-600 lowercase">{user?.email}</span>{" "}
-              you are about to place an order for{" "}
-            </p>
-            <div className="flex flex-col w-full h-full gap-4 p-2"> {cartItems.map((item) =>
-             (<OrderedItem key={item.id} item={item} />
-              ))}
+
+      <div class="relative mx-auto w-full mt-16">
+        <div class="grid min-h-screen grid-cols-10">
+       
+          <div class="relative col-span-full flex flex-col py-6 pl-8 pr-4 sm:py-12 lg:col-span-6 lg:py-24 bg-gray-50">
+            <h2 class="sr-only text-black">Order summary</h2>
+          
+            <div class="relative">
+              <ul class="space-y-5">
+              {cartItems.map((item, id) => (
+                <li key={id} class="flex justify-between border-b pb-5">
+                  <div class="inline-flex">
+                    <img src={`${SERVER_URL}/${item?.image}`} alt="" class="max-h-16" />
+                    <div class="ml-3">
+                      <p class="text-base font-semibold text-black">{item?.name}</p>
+                      <p class="text-sm font-light  text-gray-500 text-opacity-80">{item?.description}</p>
+                    </div>
+                  </div>
+                  <p class="text-sm font-semibold text-black">ksh {item?.price}</p>
+                </li>
+               ))}
+              </ul>
+
+              <div class="space-y-2 border-t">
+                <p class="flex justify-between text-lg text-black"><span>Total price:</span><span>ksh. {total} {"/="}</span></p>
+              </div>
             </div>
-          </>
-        ) : (
-          <div className="flex flex-col items-center justify-center h-96 py-12 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-2xl font-bold text-gray-900">
-              Your cart is empty
-            </h1>
-          </div>
-        )}
-      </div>
 
-      <div className="flex flex-col items-center justify-center  py-12 px-4 sm:px-6 lg:px-8">
-        <p className="text-xl font-bold text-gray-500 capitalize">
-          Your Total is Ksh.{" "}
-          <span className="text-3xl font-bold text-gray-900"> {total} {"/="}</span>{" "}
-          payable on delivery
-        </p>
-      </div>
-
-      <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md my-10">
-        <p className="text-2xl font-bold text-gray-900">Please fill in Shipping Details </p>
-
-        <form className="mt-4" onSubmit={placeOrder}>
-          <div className="form-group mb-6 p-2">
-            <label htmlFor="delivery_phone" className="text-gray-700"> Phone Number</label>
-            <input
-              type="text"
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="delivery_phone"  placeholder="e.g 254712345678" value={phoneNo}onChange={(e) => setPhoneNo(e.target.value)}
-            />
+            <div class="relative mt-10 text-black">
+              <h3 class="mb-5 text-lg font-bold capitalize">user information</h3>
+              <p class="text-sm  text-gray-500 capitalize">{user?.name}</p>
+              <p class="mt-1 text-sm  text-gray-500">{user?.email}</p>
+              <p class="mt-2 text-xs text-gray-500">Contact us now for payment related issues</p>
+            </div>
+          
           </div>
 
-          <div className="form-group mb-6 p-2">
-            <label htmlFor="delivery_address" className="text-gray-700"> Address</label>
-            <input
-              type="text"
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="delivery_address" placeholder="e.g 1234 Main St" value={address} onChange={(e) => setAddress(e.target.value)}
-            />
+          <div class="col-span-full py-6 px-4 sm:py-12 lg:col-span-4 lg:py-24">
+            <div class="mx-auto w-full max-w-lg">
+              <h1 class="relative text-2xl font-medium text-gray-700 sm:text-3xl">Checkout<span class="mt-2 block h-1 w-10 bg-teal-600 sm:w-20"></span></h1>
+              <form onSubmit={placeOrder} class="mt-10 flex flex-col space-y-4">
+                <div>
+                  <label for="email" class="text-xs font-semibold text-gray-500">Phone Number</label>
+                  <input type="text" value={phoneNo}onChange={(e) => setPhoneNo(e.target.value)} placeholder="+254-07-XXXX-XXXX" class="mt-1 block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-gray-300" />
+                </div>
+                <div class="relative">
+                  <label for="card-number" class="text-xs font-semibold text-gray-500">Address</label>
+                  <input type="text"  value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Bronx, NY 10461" class="block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 pr-10 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-gray-300" />
+                  </div>
+          
+              <p class="mt-10 text-center text-sm font-semibold text-gray-500">By placing this order you agree to the 
+               <Link to="/Terms" class="whitespace-nowrap text-teal-400 underline hover:text-teal-600">Terms and Conditions</Link>
+              </p>
+              <button type="submit" class="mt-4 inline-flex w-full items-center justify-center rounded bg-gray-600 py-2.5 px-4 text-base font-semibold tracking-wide text-white text-opacity-80 outline-none ring-offset-2 transition hover:text-opacity-100 focus:ring-2 focus:ring-gray-300 sm:text-lg">Place Order</button>
+              </form>
+            </div>
           </div>
-
-          <button type="submit"
-            className=" w-full px-6 py-3.5 bg-blue-600 text-white font-medium text-md font-bold leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Place Order
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
   );
 };
 
